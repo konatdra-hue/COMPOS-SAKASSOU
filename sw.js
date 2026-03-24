6b62f5988ea14cd598814e1cc1f9aa59d2ae4f33
sam <konatdra@gmail.com>
2026-03-24 10:06:47 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'compos-sakassou-v5';
+const ASSETS = ['/', '/index.html'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  if (e.request.url.includes('jsonbin.io') || e.request.url.includes('fonts.googleapis') || e.request.url.includes('cdnjs')) {
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
